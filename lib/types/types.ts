685ae460cd5959a742ff41d71bcce1f7a2bb08bd
Ephy Kimotho <ephyk48@gmail.com>
2025-10-20 14:00:16 +0300

    chore: added checkoutReport type

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,6 +1,14 @@
 import { getBookings } from "@/lib/actions/bookings";
 import { getInventoryItems } from "@/lib/actions/inventory"
+import { getCheckoutReports } from "../actions/checkout";
 
 
 export type Booking = Awaited<ReturnType<typeof getBookings>>[number]
 export type InvetoryItem = Awaited<ReturnType<typeof getInventoryItems>>[number]
+export type CheckoutReport = Awaited<ReturnType<typeof getCheckoutReports>>[number]
+
+export interface BookingsTableAndCardsProps {
+    bookings: Booking[];
+    setEditBooking: (booking: Booking) => void;
+    setIsDialogOpen: (open: boolean) => void;
+}
