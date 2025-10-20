a3f817abcbb4c582abd5bf2ea57f60ef44f26e4f
Ephy Kimotho <ephyk48@gmail.com>
2025-10-20 13:58:52 +0300

    chore: used provided in layout page for the dashboard route group

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,14 @@
 import { Layout } from "@/components/Layout";
+import TableModeContext from "@/components/TableModeContext";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <Layout>{children}</Layout>;
-} 
+  return (
+    <Layout>
+      <TableModeContext>{children}</TableModeContext>
+    </Layout>
+  );
+}
