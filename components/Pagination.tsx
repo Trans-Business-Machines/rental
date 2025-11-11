d8a9b1d55cde3680468bf9751b1ed640158fd442
Ephy Kimotho <ephyk48@gmail.com>
2025-11-11 22:29:50 +0300

    chore: created a resusable Pagination component

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -3,28 +3,45 @@
 import { Button } from "./ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-/* interface PaginationProps {
+interface PaginationProps {
   currentPage: string | number;
   totalPages: string | number;
-  nextPage: (page: number) => void;
-  previusPage: (page: number) => void;
-} */
+  handlePageChange: (page: number) => void;
+  hasNext: boolean;
+  hasPrev: boolean;
+}
 
-function Pagination() {
+function Pagination({
+  currentPage,
+  totalPages,
+  handlePageChange,
+  hasNext,
+  hasPrev,
+}: PaginationProps) {
   return (
     <div className="flex items-center justify-between pt-4 w-full">
-      <p className="text-sm text-muted-foreground">Page 1 of 1</p>
+      <p className="text-sm text-muted-foreground">
+        Page {currentPage} of {totalPages}
+      </p>
       <div className="flex items-center gap-2">
         <Button
           variant="outline"
-          className="px-2 bg-transparent cursor-pointer hover:ring-1 hover:ring-primary capitalize group hover:text-primary"
+          disabled={!hasPrev}
+          className="px-2 bg-transparent cursor-pointer disabled:bg-gray-200 hover:ring-1 hover:ring-primary capitalize group hover:text-primary"
+          onClick={() => handlePageChange(Math.max(Number(currentPage) - 1, 0))}
         >
           <ChevronLeft className="size-4 group-hover:text-primary" />
           previous
         </Button>
         <Button
           variant="outline"
-          className="px-2 bg-transparent cursor-pointer hover:ring-1 hover:ring-primary capitalize group hover:text-primary"
+          disabled={!hasNext}
+          className="px-2 bg-transparent cursor-pointer disabled:bg-gray-200 hover:ring-1 hover:ring-primary capitalize group hover:text-primary"
+          onClick={() =>
+            handlePageChange(
+              Math.min(Number(currentPage) + 1, Number(totalPages))
+            )
+          }
         >
           next
           <ChevronRight className="size-4 group-hover:text-primary" />
